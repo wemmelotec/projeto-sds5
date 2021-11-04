cb0f6c7fd03bb1c344a28008cbddf76c37dc43e5
wemmelotec <tonmelo@hotmail.com>
2021-11-03 23:34:59 -0300

    Pagination

---
+++ backend/src/main/java/com/devsuperior/dsvendas/service/SaleService.java
@@ -0,0 +1,35 @@
+package com.devsuperior.dsvendas.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.entities.Sale;
+import com.devsuperior.dsvendas.repositories.SaleRepository;
+import com.devsuperior.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	//implementado para evitar a busca dos vendedores repitidas vezes no banco
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable){
+		//antes de fazer a consulta eu trago os vendedores para o cache e a jpa identifica e só faz uma consulta
+		sellerRepository.findAll();
+		//recebe a entidade do banco já paginada por conta do Pageable
+		Page<Sale> result =  repository.findAll(pageable);
+		//converte para o objeto DTO
+		//como Page é uma stream já utilizo o map direto e não preciso do collect
+		return result.map(x->new SaleDTO(x));
+	}
+}
