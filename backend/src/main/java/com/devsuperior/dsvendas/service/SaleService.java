f988b9b49b1176b166c0cded8f8d4c30fab94a20
wemmelotec <tonmelo@hotmail.com>
2021-11-04 20:35:21 -0300

    Group by search

--- backend/src/main/java/com/devsuperior/dsvendas/service/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsvendas/service/SaleService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dsvendas.service;
 
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -8,28 +9,41 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.dto.SaleSuccessDTO;
+import com.devsuperior.dsvendas.dto.SaleSumDTO;
 import com.devsuperior.dsvendas.entities.Sale;
 import com.devsuperior.dsvendas.repositories.SaleRepository;
 import com.devsuperior.dsvendas.repositories.SellerRepository;
 
 @Service
 public class SaleService {
-	
+
 	@Autowired
 	private SaleRepository repository;
-	
-	//implementado para evitar a busca dos vendedores repitidas vezes no banco
+
+	// implementado para evitar a busca dos vendedores repitidas vezes no banco
 	@Autowired
 	private SellerRepository sellerRepository;
+
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable) {
+		// antes de fazer a consulta eu trago os vendedores para o cache e a jpa
+		// identifica e só faz uma consulta
+		sellerRepository.findAll();
+		// recebe a entidade do banco já paginada por conta do Pageable
+		Page<Sale> result = repository.findAll(pageable);
+		// converte para o objeto DTO
+		// como Page é uma stream já utilizo o map direto e não preciso do collect
+		return result.map(x -> new SaleDTO(x));
+	}
+
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller() {
+		return repository.amountGroupedBySeller();
+	};
 	
 	@Transactional(readOnly = true)
-	public Page<SaleDTO> findAll(Pageable pageable){
-		//antes de fazer a consulta eu trago os vendedores para o cache e a jpa identifica e só faz uma consulta
-		sellerRepository.findAll();
-		//recebe a entidade do banco já paginada por conta do Pageable
-		Page<Sale> result =  repository.findAll(pageable);
-		//converte para o objeto DTO
-		//como Page é uma stream já utilizo o map direto e não preciso do collect
-		return result.map(x->new SaleDTO(x));
-	}
+	public List<SaleSuccessDTO> successGroupedBySeller(){
+		return repository.successGroupedBySeller();
+	};
 }
