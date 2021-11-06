e09d4b76d5e8cbe452f2668c9356723d136f2c11
wemmelotec <tonmelo@hotmail.com>
2021-11-05 22:03:09 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
@@ -8,24 +9,29 @@ type ChartData = {
     series: number[];
 }
 const DonutChart = () => {
-    //forma errada
-    let chartData : ChartData = {labels: [], series: []};
 
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-            .then(response=>{
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then(response => {
                 const data = response.data as SaleSum[];
-                const myLabels = data.map(x=>x.sellerName);
-                const mySeries = data.map(x=>x.sum);
-                
-                chartData = {labels: myLabels, series: mySeries};
-                //console.log(response.data);
-                console.log(chartData);
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
+
+                setChartData({ labels: myLabels, series: mySeries });
+
             });
+    }, [])
+    //forma errada
+    //let chartData : ChartData = {labels: [], series: []};
+
+
     //const mockData = {
     //    series: [477138, 499928, 444867, 220426, 473088],
     //   labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     //}
-    
+
     const options = {
         legend: {
             show: true
@@ -33,7 +39,7 @@ const DonutChart = () => {
     }
     return (
         <Chart
-            options={{ ...options, labels: chartData.labels}}
+            options={{ ...options, labels: chartData.labels }}
             series={chartData.series}
             type="donut"
             height="240"
